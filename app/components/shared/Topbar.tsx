42056837932371598781b3917fc5094b4e5b0c02
LeeYongho91 <skyks4848@naver.com>
2024-01-29 23:57:14 +0900

    topbar 추가

--- app/components/shared/Topbar.tsx
+++ app/components/shared/Topbar.tsx
@@ -1,7 +1,12 @@
+import { SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 function Topbar() {
+
+  const isUserLogginIn = true;
+
+
   return (
   <nav className="topbar">
     <Link href="/" className="flex items-center gap-4">
@@ -9,7 +14,11 @@ function Topbar() {
       <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
     </Link>
 
-    <div className="flex items-center gap-1"></div>
+    <div className="flex items-center gap-1">
+       <div className="block md:hidden">
+            <SignedIn></SignedIn>
+       </div>
+    </div>
 
   </nav>
   )
